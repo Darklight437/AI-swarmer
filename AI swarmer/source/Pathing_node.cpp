cb258a6f6bc1a6822e6ec019a9640366dbc9d456
Darklight437 <darklight437@gmail.com>
2017-08-08 16:49:52 +1000

    work on getting the nodes to link to each other

--- AI swarmer/source/Pathing_node.cpp
+++ AI swarmer/source/Pathing_node.cpp
@@ -35,20 +35,21 @@ void Pathing_node::radiusForCheck(std::list <Pathing_node*> nodesToSearch)
             sf::Vector2f dispVect = this->m_position - (*iter)->m_position;
             
             float squaredMagnitude = (dispVect.x * dispVect.x) + (dispVect.y * dispVect.y);
+            //valid range nodes
+            if (squaredMagnitude < squaredRadius)
+            {
+                LinkedNodes.push_back(this);
+            }
             
-            
-            
-
-
-
 
         }
-    }
-    //look at the list of all nodes, (ignore self)
-    //loop over and get a vector for each from your poition (oh boy how to do that)
+    }// /for
 
-    //if the sqrt of the vector is less than the saved radius sqrt
-    //its good and you put it in a temp vector of linked nodes 
+
+    for (iter = LinkedNodes.begin(); iter != LinkedNodes.end(); iter++)
+    {
+
+    }
 
     //once all nodes have been checked go through the vector,
     //create an edge between the node if there isnt one already
