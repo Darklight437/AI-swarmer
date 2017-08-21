f115714d11ecc89b9490271c499e91bd831412ee
Darklight437 <darklight437@gmail.com>
2017-08-21 16:51:01 +1000

    comments

--- AI swarmer/source/Seek.cpp
+++ AI swarmer/source/Seek.cpp
@@ -18,7 +18,8 @@ Seek::Seek(sf::Vector2f myPosition)
 
 sf::Vector2f Seek::calculateForce()
 {
-
+    //get the vector to the target, subtract myPos
+    //normalise 
 
     return steeringForce;
 }
