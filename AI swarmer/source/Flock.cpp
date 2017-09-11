189dcf0f25ce84b81cc08ba1ff755ff7d60ac83c
Darklight437 <darklight437@gmail.com>
2017-09-11 15:23:31 +1000

    probs broken

--- AI swarmer/source/Flock.cpp
+++ AI swarmer/source/Flock.cpp
@@ -13,18 +13,39 @@ Flock::~Flock()
 
 sf::Vector2f Flock::calculateForce(sf::Vector2f myPosition, std::list<Swarmer*>neighbours)
 {
-    sf::Vector2f steeringForce;
+    sf::Vector2f steeringF;
+    sf::Vector2f separationF;
+    sf::Vector2f alignF;
+    sf::Vector2f cohereF;
+    for each (Swarmer* neighbour in neighbours)
+    {
+       separationF = separation(myPosition, neighbour->m_sprite.getPosition());
+       alignF = align(neighbour->);
+       cohereF = cohere(myPosition, neighbour->m_sprite.getPosition());
+    }
 
 
 
 
-
-
-    return steeringForce;
+    return steeringF;
 }
 
-sf::Vector2f Flock::separation()
+sf::Vector2f Flock::separation(sf::Vector2f myPosition, sf::Vector2f neighbour)
 {
+    sf::Vector2f separationForce;
+    separationForce += myPosition - neighbour;
+
+    return separationForce;
+   
+}
+
+sf::Vector2f Flock::align(sf::Vector2f myPosition, sf::Vector2f neighborV)
+{
+    sf::Vector2f alignforce;
+
+    alignforce = 
+
+
 
     return sf::Vector2f();
 }
--- AI swarmer/source/Flock.h
+++ AI swarmer/source/Flock.h
@@ -12,8 +12,8 @@ public:
     //get alist of local boids
 
     sf::Vector2f calculateForce(sf::Vector2f myPosition, std::list<Swarmer*>neighbours);
-    sf::Vector2f separation();
-    sf::Vector2f align();
-    sf::Vector2f cohere();
+    sf::Vector2f separation(sf::Vector2f myPosition, sf::Vector2f neighbourPos);
+    sf::Vector2f align(sf::Vector2f myPosition, sf::Vector2f neighbourPos);
+    sf::Vector2f cohere(sf::Vector2f myPosition, sf::Vector2f neighbourPos);
 };
 
